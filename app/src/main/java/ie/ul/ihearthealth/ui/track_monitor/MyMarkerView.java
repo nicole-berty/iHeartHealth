a4dcf0206e33727144959bd1943bfa6d002b1a3b
Nicole Berty <nicole.berty@gmail.com>
2022-03-13 23:51:31 +0000

    Add MyMarkerView class which enables clicking on data points on graph to see information about them.

---
+++ app/src/main/java/ie/ul/ihearthealth/ui/track_monitor/MyMarkerView.java
@@ -0,0 +1,43 @@
+package ie.ul.ihearthealth.ui.track_monitor;
+
+import android.content.Context;
+import android.widget.TextView;
+
+import com.github.mikephil.charting.components.MarkerView;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.highlight.Highlight;
+import com.github.mikephil.charting.utils.MPPointF;
+
+import ie.ul.ihearthealth.R;
+
+public class MyMarkerView extends MarkerView {
+    private TextView tvContent;
+
+    public MyMarkerView(Context context, int layoutResource) {
+        super(context, layoutResource);
+
+        // find your layout components
+        tvContent = (TextView) findViewById(R.id.tvContent);
+    }
+
+    // callbacks everytime the MarkerView is redrawn, can be used to update the
+// content (user-interface)
+    @Override
+    public void refreshContent(Entry e, Highlight highlight) {
+        tvContent.setText(MonitorFragment.getPointDetails((int) e.getX(), e.getY()));
+        super.refreshContent(e, highlight);
+    }
+
+    private MPPointF mOffset;
+
+    @Override
+    public MPPointF getOffset() {
+
+        if(mOffset == null) {
+            // center the marker horizontally and vertically
+            mOffset = new MPPointF(-(getWidth() / 2), -getHeight());
+        }
+
+        return mOffset;
+    }
+}
